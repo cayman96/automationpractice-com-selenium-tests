b6d8b02941f0345c43d1318a2c81f1512ab2d034
kaju <cayman96@gmail.com>
2022-09-12 19:52:58 +0200

    initial commit, header and footer locators

---
+++ page_object_pattern/locators/locators.py
@@ -0,0 +1,59 @@
+class HeaderLocators:
+    upperBanner = '//*[@id="header"]/div[1]/div/div/a/img'
+    searchBarInput = '//*[@id="search_query_top"]'
+    searchBarBtn = '//*[@id="searchbox"]/button'
+    contactUsBtn = '//*[@id="contact-link"]/a'
+    signInBtn = '//*[@id="header"]/div[2]/div/div/nav/div[1]/a'
+    logoImg = '//*[@id="header_logo"]/a/img'
+    cartBtn = '//*[@id="header"]/div[3]/div/div/div[3]/div/a'
+    womenBtn = '//*[@id="block_top_menu"]/ul/li[1]/a'
+    dressesBtn = '//*[@id="block_top_menu"]/ul/li[2]/a'
+    tShirtsBtn = '//*[@id="block_top_menu"]/ul/li[3]/a'
+
+class FooterLocators:
+    newsletterInput = '//*[@id="newsletter-input"]'
+    newsletterBtn = '//*[@id="newsletter_block_left"]/div/form/div/button'
+    footerMail = '//*[@id="block_contact_infos"]/div/ul/li[3]/span/a'
+
+
+class FooterCategoryLink:
+    footerWomenLink = '//*[@id="footer"]/div/section[2]/div/div/ul/li/a'
+    footerPrestaShopLink = '//*[@id="footer"]/div/section[4]/div/a'
+
+class FooterInformationLinks:
+    footerSpecialsLink = '//*[@id="block_various_links_footer"]/ul/li[1]/a'
+    footerNewProductsLink = '//*[@id="block_various_links_footer"]/ul/li[2]/a'
+    footerBestSellersLink = '//*[@id="block_various_links_footer"]/ul/li[3]/a'
+    footerOurStoresLink = '//*[@id="block_various_links_footer"]/ul/li[4]/a'
+    footerContactUsLink = '//*[@id="block_various_links_footer"]/ul/li[5]/a'
+    footerToSLink = '//*[@id="block_various_links_footer"]/ul/li[6]/a'
+    footerAboutLink = '//*[@id="block_various_links_footer"]/ul/li[7]/a'
+    footerSitemapLink = '//*[@id="block_various_links_footer"]/ul/li[8]/a'
+
+class FooterMyAccountLinks:
+    footerMyOrdersLink = '//*[@id="footer"]/div/section[5]/div/ul/li[1]/a'
+    footerMyCreditSlipsLink = '//*[@id="footer"]/div/section[5]/div/ul/li[2]/a'
+    footerMyAddressesLink = '//*[@id="footer"]/div/section[5]/div/ul/li[3]/a'
+    footerMyInfoLink = '//*[@id="footer"]/div/section[5]/div/ul/li[4]/a'
+
+class SocialMediaLinks:
+    facebookLink = '//*[@id="social_block"]/ul/li[1]/a'
+    twitterLink = '//*[@id="social_block"]/ul/li[2]/a'
+    youtubeLink = '//*[@id="social_block"]/ul/li[3]/a'
+    gPlusLink = '//*[@id="social_block"]/ul/li[4]/a'
+
+class MainPageLocators:
+    bigAdPrevBtn = '//*[@id="homepage-slider"]/div/div[2]/div/a[1]'
+    bigAdNextBtn = '//*[@id="homepage-slider"]/div/div[2]/div/a[2]'
+    bigAdSlider = '//*[@id="homeslider"]'
+    smallAddUpper = '//*[@id="htmlcontent_top"]/ul/li[1]/a/img'
+    smallAddLower = '//*[@id="htmlcontent_top"]/ul/li[2]/a/img'
+    popularBtn = '//*[@id="home-page-tabs"]/li[1]/a'
+    bestSellersBtn = '//*[@id="home-page-tabs"]/li[2]/a'
+    topTrendsAd = '//*[@id="htmlcontent_home"]/ul/li[1]/a/img'
+    mensCoatsJacketsAd = '//*[@id="htmlcontent_home"]/ul/li[2]/a/img'
+    womensCoatsJacketsAd = '//*[@id="htmlcontent_home"]/ul/li[3]/a/img'
+    sunglassesAd = '//*[@id="htmlcontent_home"]/ul/li[4]/a/img'
+    handbagsAd = '//*[@id="htmlcontent_home"]/ul/li[5]/a/img'
+    seleniumFrameworkBtn = '//*[@id="cmsinfo_block"]/div[1]/ul/li[2]/div/p/a'
+
